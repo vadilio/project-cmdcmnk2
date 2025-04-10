e6939facd86c1c5e7b9314824806fe4adf7dba37
Roman Goncharov <goncharov_roman@icloud.com>
2025-04-10 23:19:54 +0200

    config.py

---
+++ utils/config.py
@@ -0,0 +1,12 @@
+import re
+import pickle
+from collections import UserDict
+from datetime import datetime, timedelta
+import difflib  # Для додаткового функціоналу вгадування команд
+import os  # Для роботи з файловою системою
+
+# Параметри:
+# --- Збереження та Завантаження Даних ---
+DATA_DIR = "personal_assistant_data"  # Назва папки для даних
+CONTACTS_FILE = os.path.join(DATA_DIR, "contacts.pkl")
+NOTES_FILE = os.path.join(DATA_DIR, "notes.pkl")
