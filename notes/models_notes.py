024f4d42ca222f9700c222abf06693d93e2c8785
Iryna Gychka <iryna.gychka.triare@gmail.com>
2025-04-11 10:17:15 +0300

    Implemented class methods

--- notes/models_notes.py
+++ notes/models_notes.py
@@ -21,3 +21,27 @@ class Note:
             else set()
         )
         self.created_at = datetime.now()  # Дата створення
+
+    def add_tag(self, tag):
+        """Додає тег до нотатки."""
+        if isinstance(tag, str):
+            tag_clean = tag.strip().lower()
+            if tag_clean:  # Не додаємо порожні теги
+                self.tags.add(tag_clean)
+        else:
+            print("Помилка: Тег має бути рядком.")  # Або raise TypeError
+
+    def remove_tag(self, tag):
+        """Видаляє тег з нотатки."""
+        if isinstance(tag, str):
+            tag_clean = tag.strip().lower()
+            # discard не викликає помилку, якщо тег не знайдено
+            self.tags.discard(tag_clean)
+        else:
+            print("Помилка: Тег має бути рядком.")  # Або raise TypeError
+
+    def edit_text(self, new_text):
+        """Редагує текст нотатки."""
+        if not new_text or not isinstance(new_text, str):
+            raise ValueError("Текст нотатки не може бути порожнім рядком.")
+        self.text = new_text
